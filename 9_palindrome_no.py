3073b63fbbba247da3fc822ea72de1a007930fcb
Gabogogi <bencitymaina@gmail.com>
2024-02-26 23:13:50 +0300

    number palindrome: solution 2

--- 9_palindrome_no.py
+++ 9_palindrome_no.py
@@ -9,4 +9,17 @@ def isPalindrome(self, x):
         :type x: int
         :rtype: bool
         """
-        return(str(x) == str(x)[::-1])
+        #return(str(x) == str(x)[::-1]) #reverses string and checks if its same
+
+        # Don't convert to string
+        if x < 0:
+            return False
+
+        rev_num = 0
+        digit = 0
+
+        while(x // (10**digit) != 0):
+            rev_num = (rev_num * 10) + (x // (10**digit) % 10)
+            digit += 1
+
+        return(x == rev_num)
